1dbaf6f02a18c2b5fd7d736b9a4c192f38662546
Daniel I <exodius666@hotmail.com>
2017-11-25 02:43:00 +0100

    Added Class:User

---
+++ src/User.java
@@ -0,0 +1,31 @@
+public class User {
+    private int id, money;
+    private String name, password;
+
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.money = 500;//Arbitrary cash deposit =)
+        this.name = name;
+        this.password = password;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
