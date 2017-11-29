ca1d816155663ec83c8e18ec2fc1404356eb2150
AMPOV <anders.malkolm@gmail.com>
2017-11-29 11:59:20 +0100

    hej

--- videopoker/Hand.java
+++ videopoker/Hand.java
@@ -1,26 +1,25 @@
-package videopoker;
+package scrum;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Hand {
-	List<Card>handO = new ArrayList<>();
-	List<Card>trash = new ArrayList<>();
+	private ArrayList<Card>currentHand;
+	private ArrayList<Card>trash;
 
 	Deck d = new Deck();	
 	
 	public void fill(){
-		for(;handO.size() < 5;){
-			handO.add(d.draw());
+		for(;currentHand.size < 5;){
+			currentHand.add(d.draw());
 		}
 	}
 	public void add(){
-		handO.add(d.draw());
+		currentHand(d.draw());
 
 	}	
 	public void discard(int i){
-		trash.add(handO.get(i - 1));
-		handO.remove(i - 1);
+		currentHand.remove(i - 1);
+		trash.add(i - 1);
 	}
 	
 }
