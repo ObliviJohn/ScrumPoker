0da5ab71e6efd91f76ef3dc0eee3852db2b26695
Milad Ghasemi <miladghasemi132@hotmail.com>
2017-12-05 10:16:25 +0100

    Add update to UserToFile.java

--- src/Model/UserToFile.java
+++ src/Model/UserToFile.java
@@ -1,4 +1,5 @@
-package Model;
+package videopoker;
+
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -8,55 +9,58 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class UserToFile implements Serializable {
 
-	ArrayList<User> user = new ArrayList<>();
+ArrayList<User> user = new ArrayList<>();
 
-	public void inputUser(int bank, String name, String password) {
-
-		user.add(new User(bank, name, password));
-
+	
+	
+	public void inputUser(int bank,  String name ,String password) {
+		
+		user.add(new User(bank,name,password));
+		
 	}
-
 	public void createUser() {
-
+		
+	
+		
 		try {
-			FileOutputStream fos = new FileOutputStream(new File("userList2.txt"), true);
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			oos.writeObject(user);
-			oos.close();
-			fos.close();
-		} catch (IOException i) {
+		FileOutputStream fos = new FileOutputStream(new File("userList.txt"));
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(user);
+		oos.close();
+		fos.close();
+		}catch (IOException i ) {
 			i.printStackTrace();
 		}
-		System.out.println("Added user");
+				System.out.println("Added user");
 	}
-
-	public void readUser() throws ClassNotFoundException, IOException {
-
-		File f = new File("userList2.txt");
-		FileInputStream fis = new FileInputStream("userList2.txt");
-		ObjectInputStream oin = new ObjectInputStream(fis);
-		this.user = (ArrayList<User>) oin.readObject();
-
-		try {
-
-			if (f.exists()) {
-				User u = new User();
-				this.user.add(u);
-			}
-
-		} catch (ClassCastException cc) {
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<User> readUser() throws  ClassNotFoundException, IOException {
+		List <User> readUser = null;
+	
+		try{
+			
+			ObjectInputStream oin = new ObjectInputStream(new FileInputStream("userList.txt"));
+			readUser = (List<User>) oin.readObject();
+	
+		
+			
+		} catch(ClassCastException cc) {
 			cc.printStackTrace();
-
-		}
-
-		for (User u : user) {
-			System.out.println(u);
-		}
-		System.out.println(oin.toString());
+			
+		}catch(Exception e) {}
+		System.out.println(readUser.toString());
+		return readUser;
+		
+		
 
 	}
 
+	
 }
