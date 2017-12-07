15dfc38f85cafce5dfcc7f0f9ec395cdbaa335e6
John Niklasson <John_niklasson@hotmail.com>
2017-12-07 11:48:35 +0100

    add more stats, fix replay

--- src/Model/Statistics.java
+++ src/Model/Statistics.java
@@ -4,18 +4,18 @@ public class Statistics {
 
 	public Double[] getStats(int i, int j){
 
-		int x = i;//handvï¿½rde
-		int y = j;//dragvï¿½rde
+		int x = i;//handvärde
+		int y = j;//dragvärde
 		
 		if(x==0 && y == 0) { // Inga kombinationer
 			Double[] odds = new Double[] {42.3d, 4.8d, 2.1d, 0.39d, 0.20d, 0.14d, 0.024d, 0.0014d, 0.00015d};
 			return odds;
 		}
 		else if(x==1 && y == 0) {	// Par
-			Double[] odds = new Double[] {0d, 15.8, 11.4, 0d, 0d, 1.0, 0.28, 0d, 0d};
+			Double[] odds = new Double[] {0d, 15.9, 11.4, 0d, 0d, 1.0, 0.28, 0d, 0d};
 			return odds;
 		}
-		else if(x==2 && y == 0) { // Tvï¿½ Par
+		else if(x==2 && y == 0) { // Två Par
 			Double[] odds = new Double[] {0d, 0d, 0d, 0d, 0d, 8.5, 0d, 0d, 0d};
 			return odds;
 		}
@@ -23,44 +23,44 @@ public class Statistics {
 			Double[] odds = new Double[] {0d, 0d, 0d, 0d, 0d, 6.1, 4.3, 0d, 0d};
 			return odds;
 		}
-		else if(x==0 && y==1) { // Hï¿½lstege
+		else if(x==0 && y==1) { // Hålstege
 			Double[] odds = new Double[] {25.5d, 0d, 0d, 8.5d, 0d, 0d, 0d, 0d, 0d};
 			return odds;
 		}
-		else if(x==0 && y==2) { // ï¿½ppet stegdrag
+		else if(x==0 && y==2) { // Öppet stegdrag
 			Double[] odds = new Double[] {25.5d, 0d, 0d, 17.0d, 0d, 0d, 0d, 0d, 0d};
 			return odds;
 		}
-		else if(x==0 && y==3) { // Fï¿½rgdrag
+		else if(x==0 && y==3) { // Färgdrag
 			Double[] odds = new Double[] {0d, 0d, 0d, 0d, 19.1d, 0d, 0d, 0d, 0d};
 			return odds;
 		}
-		else if(x==0 && y==4) { // Kombinerat drag hï¿½lstegdrag och fï¿½rgdrag
+		else if(x==0 && y==4) { // Kombinerat drag hålstegdrag och färgdrag
 			Double[] odds = new Double[] {25.5d, 0d, 0d, 8.5d, 19.1d, 0d, 0d, 0d, 0d};
 			return odds;
 		}
-		else if(x==0 && y==5) { // Kombinerat ï¿½ppet stegdrag och fï¿½rgdrag
+		else if(x==0 && y==5) { // Kombinerat öppet stegdrag och färgdrag
 			Double[] odds = new Double[] {25.5d, 0d, 0d, 17.0d, 19.1d, 0d, 0d, 0d, 0d};
 			return odds;
 		}
 		else if(x==1 && y==1) {
-			Double[] odds = new Double[] {0d, 0d, 0d, 8.5d, 0d, 0d, 0d, 0d, 0d};
+			Double[] odds = new Double[] {0d, 15.9d, 11.4d, 8.5d, 0d, 0d, 0d, 0d, 0d};
 			return odds;
 		}
 		else if(x==1 && y==2) {
-			Double[] odds = new Double[] {0d, 0d, 0d, 17.0d, 0d, 0d, 0d, 0d, 0d};
+			Double[] odds = new Double[] {0d, 15.9d, 11.4d, 17.0d, 0d, 0d, 0d, 0d, 0d};
 			return odds;
 		}		
 		else if(x==1 && y==3) {
-			Double[] odds = new Double[] {0d, 0d, 0d, 0d, 19.1d, 0d, 0d, 0d, 0d};
+			Double[] odds = new Double[] {0d, 15.9d, 11.4d, 0d, 19.1d, 0d, 0d, 0d, 0d};
 			return odds;
 		}		
 		else if(x==1 && y==4) {
-			Double[] odds = new Double[] {0d, 0d, 0d, 8.5d, 19.1d, 0d, 0d, 0d, 0d};
+			Double[] odds = new Double[] {0d, 15.9d, 11.4d, 8.5d, 19.1d, 0d, 0d, 0d, 0d};
 			return odds;
 		}		
 		else if(x==1 && y==5) {
-			Double[] odds = new Double[] {0d, 0d, 0d, 17.0d, 19.1d, 0d, 0d, 0d, 0d};
+			Double[] odds = new Double[] {0d, 15.9d, 11.4d, 17.0d, 19.1d, 0d, 0d, 0d, 0d};
 			return odds;
 		}		
 		else {
